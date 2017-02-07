721b37f271d4f8747d4268cc7e248b4081ee5f22
qwerty1917 <hmp0077@gmail.com>
2017-02-06 21:32:43 -0800

    Project forked from appkr/dotfiles

---
+++ Brewfile
@@ -0,0 +1,188 @@
+#-------------------------------------------------------------------------------
+# Taps
+#-------------------------------------------------------------------------------
+
+tap 'caskroom/cask'
+tap 'caskroom/fonts'
+tap 'caskroom/versions'
+tap 'homebrew/dupes'
+tap 'homebrew/php'
+tap 'homebrew/services'
+tap 'homebrew/versions'
+
+#-------------------------------------------------------------------------------
+# Make sure apps get installed in system Applications dir
+#-------------------------------------------------------------------------------
+
+cask_args appdir: '/Applications'
+
+#-------------------------------------------------------------------------------
+# Install ZSH
+#-------------------------------------------------------------------------------
+
+brew 'zsh'
+brew 'zsh-completions'
+
+#-------------------------------------------------------------------------------
+# Install GNU core utilities (those that come with OS X are outdated)
+#-------------------------------------------------------------------------------
+
+brew 'coreutils'
+
+#-------------------------------------------------------------------------------
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+#-------------------------------------------------------------------------------
+
+brew 'findutils'
+
+#-------------------------------------------------------------------------------
+# Install Bash 4
+#-------------------------------------------------------------------------------
+
+brew 'bash'
+
+#-------------------------------------------------------------------------------
+# Install more recent versions of some OS X tools
+#-------------------------------------------------------------------------------
+
+brew 'homebrew/dupes/grep'
+
+#-------------------------------------------------------------------------------
+# Install Binaries
+#-------------------------------------------------------------------------------
+
+brew 'asciinema'
+# brew 'awscli'
+brew 'aws-elasticbeanstalk'
+# brew 'autoconf'
+brew 'ffmpeg'
+brew 'git'
+brew 'git-flow'
+# brew 'git-stree' # deprecated
+brew 'highlight'
+brew 'htop'
+brew 'httpie'
+brew 'hub'
+brew 'jmeter'
+# brew 'latex2html'
+brew 'libav'
+brew 'mackup'
+brew 'nano'
+brew 'node@6', args: ['with-full-icu', 'with-npm']
+brew 'openssl'
+brew 'pandoc'
+brew 'tcpdump'
+brew 'thrift'
+brew 'tree'
+# brew 'trash'
+brew 'wget'
+brew 'youtube-dl'
+
+#-------------------------------------------------------------------------------
+# Development-PHP
+# @see $ brew info php71, which reads...
+# With the release of macOS Sierra the Apache module is now not built by default. 
+# If you want to build it on your system you have to install php with the 
+# --with-httpd24 option. See  brew options php71  for more details.
+#-------------------------------------------------------------------------------
+
+brew 'php71' #, args: ['without-apache']
+brew 'php71-intl'
+brew 'php71-mecab'
+brew 'php71-redis'
+brew 'php71-xdebug'
+brew 'phpdocumentor'
+brew 'composer'
+brew 'phpunit'
+brew 'php-cs-fixer'
+
+#-------------------------------------------------------------------------------
+# Development-Ruby
+#-------------------------------------------------------------------------------
+
+brew 'ruby'
+brew 'rbenv'
+
+#-------------------------------------------------------------------------------
+# Development-Python (includes pip, easy_install)
+#-------------------------------------------------------------------------------
+
+brew 'python', args:['with-sphinx-doc']
+brew 'python3', args:['with-sphinx-doc']
+brew 'pyenv-virtualenv'
+
+#-------------------------------------------------------------------------------
+# Development-Go
+#-------------------------------------------------------------------------------
+
+brew 'golang'
+
+#-------------------------------------------------------------------------------
+# Development-Database
+#-------------------------------------------------------------------------------
+
+# brew 'mariadb'
+brew 'mysql'
+brew 'sqlite'
+
+#-------------------------------------------------------------------------------
+# Apps
+#-------------------------------------------------------------------------------
+
+# cask 'adapter'
+# cask 'atom'
+# cask 'balsamiq-mockups'
+# cask 'calibre'
+cask 'docker'
+# cask 'docker-toolbox'
+cask 'evernote'
+cask 'firefox'
+# cask 'github-desktop'
+# cask 'gitkraken'
+# cask 'goofy'
+cask 'google-chrome'
+cask 'google-drive'
+cask 'intellij-idea'
+cask 'iterm2'
+# cask 'mactex'
+cask 'moom'
+# cask 'movist'
+# cask 'pgadmin4'
+cask 'phpstorm'
+cask 'psequel'
+cask 'obs'
+cask 'rdm'
+cask 'screenflow'
+# cask 'screenhero'
+cask 'sequel-pro'
+# cask 'skype'
+cask 'slack'
+cask 'sublime-text'
+# cask 'textual'
+# cask 'tower'
+# cask 'transmit'
+# cask 'vagrant'
+# cask 'virtualbox'
+# cask 'virtualbox-extension-pack'
+# cask 'vmware-fusion7'
+cask 'wireshark'
+# cask 'wkhtmltopdf'
+
+#-------------------------------------------------------------------------------
+# Quicklook
+#-------------------------------------------------------------------------------
+
+# cask 'qlcolorcode'
+# cask 'qlmarkdown'
+# cask 'quicklook-json'
+# cask 'quicklook-csv'
+# cask 'qlstephen'
+
+#-------------------------------------------------------------------------------
+# Fonts
+#-------------------------------------------------------------------------------
+
+# cask 'font-sauce-code-powerline'
+cask 'font-source-code-pro'
+# cask 'font-source-sans-pro'
+# cask 'font-source-serif-pro'
---
+++ bootstrap.sh
@@ -0,0 +1,163 @@
+#!/bin/sh
+
+#-------------------------------------------------------------------------------
+# Thanks Maxime Fabre! https://speakerdeck.com/anahkiasen/a-storm-homebrewin
+# Thanks Mathias Bynens! https://mths.be/osx
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Set temporary variable
+#-------------------------------------------------------------------------------
+
+DOTFILES=$HOME/dotfiles
+
+#-------------------------------------------------------------------------------
+# Update dotfiles itself first
+#-------------------------------------------------------------------------------
+
+[ -d "$DOTFILES/.git" ] && git --work-tree="$DOTFILES" --git-dir="$DOTFILES/.git" pull origin master
+
+#-------------------------------------------------------------------------------
+# Check for Homebrew and install if we don't have it
+#-------------------------------------------------------------------------------
+
+if test ! $(which brew); then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+#-------------------------------------------------------------------------------
+# Update Homebrew recipes
+#-------------------------------------------------------------------------------
+
+brew update
+
+#-------------------------------------------------------------------------------
+# Install all our dependencies with bundle (See Brewfile)
+#-------------------------------------------------------------------------------
+
+brew tap homebrew/bundle
+brew bundle --file=$DOTFILES/Brewfile # Install binary & applications
+brew cleanup
+brew cask cleanup
+
+#-------------------------------------------------------------------------------
+# Launch sublime in command line
+# Now this command is built-in
+#-------------------------------------------------------------------------------
+
+# ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+
+#-------------------------------------------------------------------------------
+# Install global Git configuration
+#-------------------------------------------------------------------------------
+
+ln -nfs $DOTFILES/.gitconfig $HOME/.gitconfig
+git config --global core.excludesfile $DOTFILES/.gitignore_global
+git config --global user.name "foo"
+git config --global user.email "foo@bar.com"
+
+#-------------------------------------------------------------------------------
+# Make ZSH the default shell environment
+#-------------------------------------------------------------------------------
+
+chsh -s $(which zsh)
+
+#-------------------------------------------------------------------------------
+# Install Oh-my-zsh
+#-------------------------------------------------------------------------------
+
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Install cobalt2 theme
+# wget https://raw.githubusercontent.com/wesbos/Cobalt2-iterm/master/cobalt2.zsh-theme -O $HOME/.oh-my-zsh/themes/cobalt2.zsh-theme
+
+# Install Powerline theme
+wget https://raw.githubusercontent.com/jeremyFreeAgent/oh-my-zsh-powerline-theme/master/powerline.zsh-theme -O $HOME/.oh-my-zsh/themes/powerline.zsh-theme
+git clone git@github.com:powerline/fonts.git && bash fonts/install.sh
+sleep 3
+rm -rf fonts
+
+#-------------------------------------------------------------------------------
+# Install & execute profile
+#-------------------------------------------------------------------------------
+
+# Always prefer dotfiles' .zshrc
+# [ ! -f $HOME/.zshrc ] && ln -nfs $DOTFILES/.zshrc $HOME/.zshrc
+ln -nfs $DOTFILES/.zshrc $HOME/.zshrc
+
+#-------------------------------------------------------------------------------
+# Install Mackup config
+#-------------------------------------------------------------------------------
+
+[ ! -f $HOME/.mackup.cfg ] && ln -nfs $DOTFILES/.mackup.cfg $HOME/.mackup.cfg
+
+#-------------------------------------------------------------------------------
+# Install .nanorc
+#-------------------------------------------------------------------------------
+
+ln -nfs $DOTFILES/.nanorc $HOME/.nanorc
+
+#-------------------------------------------------------------------------------
+# Install Composer
+#-------------------------------------------------------------------------------
+
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+#-------------------------------------------------------------------------------
+# Install global Composer packages
+#-------------------------------------------------------------------------------
+
+/usr/local/bin/composer global require laravel/installer laravel/envoy laravel/valet tightenco/jigsaw
+
+#-------------------------------------------------------------------------------
+# Install Laravel Valet
+# IMPORTANT NOTE
+# For valet to work correctly, php71 & nginx must be run as sudo privileges.
+# # brew services start php71 && brew services nginx
+#-------------------------------------------------------------------------------
+
+$HOME/.composer/vendor/bin/valet install
+cd $HOME/workspace && $HOME/.composer/vendor/bin/valet park
+
+#-------------------------------------------------------------------------------
+# Install Homestead Repo & Add vagrant box
+# Commented out on behalf of Docker
+#-------------------------------------------------------------------------------
+
+# git clone git@github.com:laravel/homestead.git $HOME/Homestead
+# cd $HOME/Homestead && bash ./init.sh
+# [[ $(basename $(pwd)) != "Homestead" ]] && cd $HOME/Homestead; vagrant box add laravel/homestead
+
+#-------------------------------------------------------------------------------
+# Install global Node packages
+#-------------------------------------------------------------------------------
+
+npm install gulp-cli gulp yo http-server nodemon --global --save
+# npm install gitbook-cli --global --save
+
+#-------------------------------------------------------------------------------
+# Install Rails & Jekyll
+#-------------------------------------------------------------------------------
+
+gem install rails jekyll
+
+#-------------------------------------------------------------------------------
+# Install AWS Shell
+# Commented out because I prefer brew version of aws-cli
+#-------------------------------------------------------------------------------
+
+# pip install aws-shell
+
+#-------------------------------------------------------------------------------
+# Source profile
+#-------------------------------------------------------------------------------
+
+source $HOME/.zshrc
+
+#-------------------------------------------------------------------------------
+# Set OS X preferences
+# We will run this last because this will reload the shell
+#-------------------------------------------------------------------------------
+
+source $DOTFILES/.osx
